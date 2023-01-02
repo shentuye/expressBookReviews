607af2a6f2a2af00309d376545950ee7d295f385
Shen <shen@ifkasolution.com>
2023-01-02 14:21:20 +0100

    Async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,6 +5,49 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function getBookList(){
+  return new Promise((resolve,reject)=>{
+    resolve(books);
+  })
+}
+
+function getFromISBN(isbn){
+  let book_ = books[isbn];  
+  return new Promise((resolve,reject)=>{
+    if (book_) {
+      resolve(book_);
+    }else{
+      reject("Unable to find book!");
+    }    
+  })
+}
+
+function getFromAuthor(author){
+  let output = [];
+  return new Promise((resolve,reject)=>{
+    for (var isbn in books) {
+      let book_ = books[isbn];
+      if (book_.author === author){
+        output.push(book_);
+      }
+    }
+    resolve(output);  
+  })
+}
+
+function getFromTitle(title){
+  let output = [];
+  return new Promise((resolve,reject)=>{
+    for (var isbn in books) {
+      let book_ = books[isbn];
+      if (book_.title === title){
+        output.push(book_);
+      }
+    }
+    resolve(output);  
+  })
+}
+
 const doesExist = (username)=>{
   let userswithsamename = users.filter((user)=>{
     return user.username === username
@@ -29,52 +72,42 @@ public_users.post("/register", (req,res) => {
       return res.status(404).json({message: "User already exists!"});    
     }
   } 
-  return res.status(404).json({message:  "Unable to register user."+username});
+  return res.status(404).json({message:  "Unable to register user." + username});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books, null, 4));
+  getBookList().then(
+    (bk)=>res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send("denied")
+  );  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  let book_ = books[isbn];
-  if (book_) {
-    res.send(JSON.stringify(book_, null, 4));
-  }else{
-    res.send("Unable to find book!");
-  }
+  getFromISBN(isbn).then(
+    (bk)=>res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send(error)
+  )
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  let output = [];
   const author = req.params.author;
-  for (var isbn in books) {
-    let book_ = books[isbn];
-    if (book_.author === author){
-      output.push(book_);
-    }
-  }
-  res.send(JSON.stringify(output, null, 4));
+  getFromAuthor(author)
+  .then(
+    result =>res.send(JSON.stringify(result, null, 4))
+  );
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  let output = [];
   const title = req.params.title;
-  for (var isbn in books) {
-    let book_ = books[isbn];
-    if (book_.title === title){
-      output.push(book_);
-    }
-  }
-  res.send(JSON.stringify(output, null, 4));
+  getFromTitle(title)
+  .then(
+    result =>res.send(JSON.stringify(result, null, 4))
+  );
 });
 
 //  Get book review
